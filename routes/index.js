b01295f8954e241feeb4fa2c976dbf0791e76f77
Jay Taylor <jaytdev@gmail.com>
2017-08-30 14:02:12 -0500

    Update routes
    - Add an example route "/reversed" to understand params
    - Update the index route to render the "hello" template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  // res.json({ name: 'Jay', age: 32, cool: true })
+  // res.send(req.query.name)
+  // res.json(req.query)
+  const name = req.query.name || 'world'
+
+  res.render('hello', { name })
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reversed = [...req.params.name].reverse().join('')
+
+  res.send(reversed)
+})
 
 module.exports = router;
